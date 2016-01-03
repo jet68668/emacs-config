5128f1731eece0c5c1238f060d57f88246965f05
JulienSong <songjn@qq.com>
2016-01-03 14:15:37 +0800

    some new plugin update,add linum-mode to defaut

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-:(setq inferior-lisp-program "/usr/local/bin/sbcl")
+(setq inferior-lisp-program "/usr/local/bin/sbcl")
 (add-to-list 'load-path "~/.emacs.d/slime/")
 (require 'slime-autoloads)
 (slime-setup '(slime-fancy))
@@ -19,5 +19,23 @@
 
 (add-to-list 'load-path "~/.emacs.d/my-config/")
 ;;use latex to export pdf file in org-mode
+(setq org-log-done 'time)
 (load "my-org-latex")
 (load "my-test")
+
+;;ace-windows
+(global-set-key (kbd "M-p") 'ace-window)
+
+;;org tags
+;;(setq org-todo-keywords
+;;      '((sequence "TODO(t!)" "NEXT(n)" "WAITING(w)" "HOLD(h)" "DELAY(e)" "|" "CANCEL(c@/!)" "DONE(d@/!)")
+;;	))
+;;agenda
+(setq org-agenda-files (quote ("~/.org/gtd-org/tasks.org")))
+(global-set-key (kbd "C-c a") 'org-agenda)
+
+(defun gtd()
+  (interactive)
+  (find-file "~/.org/gtd-org/tasks.org"))
+;;show line num
+(global-linum-mode t)
